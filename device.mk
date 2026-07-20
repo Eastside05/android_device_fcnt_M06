c1d1a8c7fb6eb96e43f8bf80a0a4f7ef687bc07b
Eastside05 <eastside05@proton.me>
2026-07-20 11:06:33 +0000

    M06: Create WLAN firmware symlinks
    Change-Id: Ia3df8787384883884808fdf9d7b2b34dc0f7466f

--- device.mk
+++ device.mk
@@ -53,5 +53,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
 
+# WiFi firmware symlink
+PRODUCT_PACKAGES += \
+    firmware_wlanmdsp.otaupdate_symlink \
+    firmware_wlan_mac.bin_symlink \
+    firmware_WCNSS_qcom_cfg.ini_symlink
+
 # Inherit from the proprietary files makefile.
 $(call inherit-product, vendor/fcnt/M06/M06-vendor.mk)
