b22ef9bc2373f88fff6e3039ba8a553d6ccaf229
Eastside05 <eastside05@proton.me>
2026-07-23 13:34:24 +0000

    M06: Build NXP NFC HAL
    Change-Id: I02a3e1ee9c0731f5ceeb940c4a3b1b1ea2ba2302

--- device.mk
+++ device.mk
@@ -110,6 +110,12 @@ PRODUCT_PACKAGES += \
     IPACM_cfg.xml \
     IPACM_Filter_cfg.xml
 
+# NFC
+PRODUCT_PACKAGES += \
+    android.hardware.nfc-service.nxp \
+    com.android.nfc_extras \
+    Tag
+
 # Partitions
 PRODUCT_PACKAGES += \
     vendor_bt_firmware_mountpoint \
